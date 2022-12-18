6c9425a19239dc24dcb5ec967ae5592a8bdd623c
Praise Abeti <praiseabeti@gmail.com>
2022-12-18 18:33:05 -0300

    testing aos.scroll

---
+++ javascript/AOS.js
@@ -0,0 +1,10 @@
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  debounceDelay: 100, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 299, // the delay on throttle used while scrolling the page (advanced)
+  
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1100, // values from 0 to 3000, with step 50ms
+});
